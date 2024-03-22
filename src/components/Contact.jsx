d29698c8a721e93f47ef88722c45a9719383b59e
humphreynjuguna <humphreynjuguna98@gmail.com>
2024-03-22 12:52:20 +0300

    added a margin between work and contacts

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div name='contact' className='w-full h-screen bg-[#070708] flex justify-center items-center p-4'>
+    <div name='contact' className='w-full mt-10 h-screen bg-[#070708] flex justify-center items-center p-4'>
         <form method='POST' action="https://getform.io/f/dddfdeca-ebdb-4820-8373-adc0fd42cc87" className='flex flex-col max-w-[600px] w-full'>
             <div className='pb-8'>
                 <p className='text-4xl font-bold inline border-b-4 border-blue-600 text-gray-300'>Contact</p>
